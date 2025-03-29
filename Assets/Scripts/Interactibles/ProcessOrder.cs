62a5ca22d084069799f59f0f9aedf277b11ea83c
Borovail <skorikdaniil17@gmail.com>
2025-03-29 21:32:46 +0200

    order system

--- Assets/Scripts/Interactibles/ProcessOrder.cs
+++ Assets/Scripts/Interactibles/ProcessOrder.cs
@@ -7,23 +7,39 @@ namespace Interactibles
     public class ProcessOrder : MonoBehaviour, IInteractable
     {
         [SerializeField] private Text _orderText;
-
-        [SerializeField] private Transform PlateSpawnPoint;
         [SerializeField] private GameObject _plate;
         [SerializeField] private ParticleSystem _sellOrderParticles;
 
+        [SerializeField] private AudioClip _takeOrderSound;
+        [SerializeField] private AudioClip _sellOrderSound;
+
+        private AudioSource _audioSource;
+        private bool _plateSpawned;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
         public void Interact()
         {
-            if (_plate.transform.position != PlateSpawnPoint.position)
+            if (!_plateSpawned)
             {
                 _orderText.text = "Cheese burger x1";
-                _plate.transform.position = PlateSpawnPoint.position;
+                _plate.SetActive(true);
+                _plate.transform.localPosition = new Vector3(0, 1f, 0);
+                _audioSource.PlayOneShot(_takeOrderSound);
             }
             else
             {
+                _orderText.text = "";
                 _sellOrderParticles.Play();
-                _plate.transform.position = new Vector3(999, 999, 999);
+                _plate.SetActive(false);
+                _audioSource.PlayOneShot(_sellOrderSound);
             }
+
+            _plate.SetActive(!_plateSpawned);
+            _plateSpawned = !_plateSpawned;
         }
     }
 }
