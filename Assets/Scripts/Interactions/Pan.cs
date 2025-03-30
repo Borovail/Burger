08aba462624f2144a3e7fa6a76208f523fe9973f
IvanMudrak <46427131+IvanMudrak159@users.noreply.github.com>
2025-03-30 15:59:44 +0200

    Revert "Merge branch 'master' of https://github.com/Borovail/Burger"
    This reverts commit 7c07e716a0242a6bfc1d3365219892d552efca17, reversing
    changes made to 185fffac719468fb7f3b4cec6d1c0c3affc9774b.

---
+++ Assets/Scripts/Interactions/Pan.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interactions
+{
+    public class Pan : MonoBehaviour,IInteractableHighlight
+    {
+        public Transform Center;
+        private MeshRenderer _renderer;
+        private Meat _meat;
+        private Highlightable _highlightable;
+
+        private void Awake()
+        {
+            _highlightable = GetComponent<Highlightable>();
+            _renderer = GetComponent<MeshRenderer>();
+        }
+
+
+        public void StartСooking(Meat meat)
+        {
+            _meat = meat;
+            _meat.GetComponent<Collider>().enabled = false;
+            _meat.GetComponent<Rigidbody>().isKinematic = true;
+
+            _meat.transform.SetParent(Center);
+            _meat.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+
+        public Meat StopCooking()
+        {
+            _meat.GetComponent<Collider>().enabled = true;
+            _meat.transform.SetParent(null);
+            var meat = _meat;
+            _meat = null;
+            return meat;
+        }
+
+        public Highlightable GetHighlighter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CanInteract(GameObject item)
+        {
+            if (item == null && _meat != null) return true;
+
+            if (_meat == null && item != null && item.TryGetComponent(out Meat meat)) return true;
+
+            return false;
+        }
+
+        public GameObject Interact(GameObject item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
